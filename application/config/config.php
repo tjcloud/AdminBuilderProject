32ff335f1669b3c6c5456a570d7d4665d08c1d19
tjcloud <39192296+tjcloud@users.noreply.github.com>
2020-02-05 12:28:34 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined("BASEPATH") OR exit("No direct script access allowed");
 
-$config["base_url"] = "http://localhost/AdminBuilder/2";
+$config["base_url"] = "http://localhost/AdminBuilder/1";
 
 $config["index_page"] = "index.php";
 
